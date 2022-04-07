03be7f810990b10d63a8d49528e369773a13e9d4
Kinga Kohari <kinguca@yahoo.com>
2022-04-07 15:27:41 +0200

    React installed

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+const text = "Hello"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        {text}
+        </p>
+        <p>Valami szöveg</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
